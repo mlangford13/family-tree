662f5d2dcae95ae8d1f3a0f6f13f4b031b5d7386
rubot42 <rubot42@gmail.com>
2018-02-12 00:12:17 -0500

    - added testing to file being tested rather than in a seperate file. - added many tests - fixed method birthBeforeDeath so it passed tests - removed old testing file

--- es05test.py
+++ es05test.py
@@ -1,32 +1,21 @@
-# fix data
+# es05 test
+# birth before death
 from mongoengine import *
 from dbDef import *
+from fixData import *
 import unittest
 import datetime
-
-connectToMongoDB()
-
-# birth before death es05
-# takes an Indi and returns true if the birth is before the death
-# or if they're still alive
-def birthBeforeDeath(x):
-    if(x.death is not None):return(x.birth<=x.death)
-    else:return(True)
-
-# removes indis that don't pass test
-for i in Indi.objects:
-    if(not birthBeforeDeath(i)):i.delete()
 
 class MyTests(unittest.TestCase):
     def test_es05(self):
         # year month day
         # dates are earliest -> latest
         # d0 is lack of date
-        d1 = datetime.date(1990,1,1)             # base
-        d2 = datetime.date(1990,1,2)             # later day
-        d3 = datetime.date(1990,2,1)             # later month
-        d4 = datetime.date(2010,1,1)             # later year
-        d5 = datetime.date(2011,6,5)             # later all
+        d1 = datetime.date(1990,1,1) # base
+        d2 = datetime.date(1990,1,2) # later day
+        d3 = datetime.date(1990,2,1) # later month
+        d4 = datetime.date(2010,1,1) # later year
+        d5 = datetime.date(2011,6,5) # later all
         i0 = Indi(pid='test1',birth=d1)          # still alive
         i1 = Indi(pid='test2',birth=d1,death=d2) # later day
         i2 = Indi(pid='test3',birth=d1,death=d3) # later month
@@ -36,7 +25,7 @@ class MyTests(unittest.TestCase):
         i6 = Indi(pid='test3',birth=d3,death=d1) # earlier month
         i7 = Indi(pid='test4',birth=d4,death=d1) # earlier year
         i8 = Indi(pid='test5',birth=d5,death=d1) # earlier all
-        i9 = Indi(pid='test',birth=d1,death=d1)  # same birth and death
+        i9 = Indi(pid='test',birth=d1,death=d1)   # same birth and death
         self.assertTrue(birthBeforeDeath(i0))
         self.assertTrue(birthBeforeDeath(i1))
         self.assertTrue(birthBeforeDeath(i2))
@@ -49,3 +38,4 @@ class MyTests(unittest.TestCase):
         self.assertTrue(birthBeforeDeath(i9))
     if __name__ == '__main__':
         unittest.main()
+
