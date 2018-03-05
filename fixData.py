db6b2fee73a9215ee3e0bd3daef4c722139fe943
rubot42 <rubot42@gmail.com>
2018-03-05 13:44:26 -0500

    update to fixData file to actually remove bad indis and associated data

--- fixData.py
+++ fixData.py
@@ -17,32 +17,78 @@ connectToMongoDB()
 #for i in Indi.objects:
 #    if(not birthBeforeDeath(i)):i.delete()
 
+def findBad():
+    badIds = []
+    for i in Indi.objects:
+        valid = True
+        # US01 date before today
+        if(not dateBeforeToday(i)):
+            valid = False
+        # US02 Birth before marriage
+        if(not birthBeforeMarriage(i)):
+            valid = False
+        # US03 Birth before death
+        if(not birthBeforeDeath(i)):
+            valid = False
+        # US04 Marriage before divorce
+        if(not marriageBeforeDivorce(i)):
+            valid = False
+        # US05 Marriage before death
+        if(not marriageBeforeDeath(i)):
+            valid = False
+        # US06 Divorce before death
+        if(not divorceBeforeDeath(i)):
+            valid = False
+        # US07 Less than 150 years old
+        if(not isLessThan150(i)):
+            valid = False
+        if not valid:
+            if i.pid not in badIds:
+                badIds.append(i.pid)
+    return badIds
 
-deletedIds = []
-for i in Indi.objects:
-    valid = True
-    # US01 date before today
-    if(not dateBeforeToday(i)):
-        valid = False
-    # US02 Birth before marriage
-    if(not birthBeforeMarriage(i)):
-        valid = False
-    # US03 Birth before death
-    if(not birthBeforeDeath(i)):
-        valid = False
-    # US04 Marriage before divorce
-    if(not marriageBeforeDivorce(i)):
-        valid = False
-    # US05 Marriage before death
-    if(not marriageBeforeDeath(i)):
-        valid = False
-    # US06 Divorce before death
-    if(not divorceBeforeDeath(i)):
-        valid = False
-    # US07 Less than 150 years old
-    if(not isLessThan150(i)):
-        valid = False
-    if not valid:
-        if i.pid not in deletedIds:
-            deletedIds.append(i.pid)
-print(deletedIds)
+# removes ids from records
+# takes the list of ids
+def removeIds(idList):
+    for i in Indi.objects:
+        # children
+        badChildren = []
+        if i.children != []:
+            for pid in i.children:
+                if pid in idList:
+                    badChildren.append(pid)
+        for child in badChildren:
+            i.children.remove(child)
+
+        # marriages
+        for pid in i.marriages:
+            if pid in idList:
+                i.marriages.pop(pid)
+        # divorces
+        for pid in i.divorces:
+            if pid in idList:
+                i.divorces.pop(pid)
+        if i.pid in idList:
+            i.delete()
+
+    for f in Fam.objects:
+        # hid
+        if f.hid in idList: f.hid = ''
+        # wid
+        if f.wid in idList: f.wid = ''
+
+        badChildren = []
+        # children
+        if f.children != []:
+            for pid in f.children:
+                if pid in idList:
+                    badChildren.append(pid)
+        for child in badChildren:
+            f.children.remove(child)
+
+badIds = findBad()
+print(badIds)
+while badIds != []:
+    removeIds(badIds)
+    badIds = findBad()
+    print(badIds)
